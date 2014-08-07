ca4eae0c07d12c25542d1085ef43e6442e23344d
amaier1 <andreas.maier@asideas.de>
2014-08-07 15:56:12 +0200

    Implemented change of parameters. If no starttime and endtime is given, the script will conveniently search for rooms available from now to now+1h.

--- find_available_room.py
+++ find_available_room.py
@@ -11,12 +11,18 @@ from string import Template
 import xml.etree.ElementTree as ET
 import csv, codecs
 import argparse
+import datetime
+
+now = datetime.datetime.now().replace(microsecond=0)
+starttime_default = now.isoformat()
+end_time_default = None
 
 parser = argparse.ArgumentParser()
-parser.add_argument("starttime", help="Starttime e.g. 2014-07-02T11:00:00")
-parser.add_argument("endtime", help="Endtime e.g. 2014-07-02T12:00:00")
 parser.add_argument("-url","--url", help="url for exhange server, e.g. 'https://mail.domain.com/ews/exchange.asmx'.",required=True)
 parser.add_argument("-u","--user", help="user name for exchange/outlook",required=True)
+parser.add_argument("-start","--starttime", help="Starttime e.g. 2014-07-02T11:00:00 (default = now)", default=starttime_default)
+parser.add_argument("-end","--endtime", help="Endtime e.g. 2014-07-02T12:00:00 (default = now+1h)", default=end_time_default)
+#parser.add_argument("-n","--now", help="Will set starttime to now and endtime to now+1h", action="store_true")
 parser.add_argument("-f","--file", help="csv filename with rooms to check (default=rooms.csv). Format: Name,email",default="rooms.csv")
 
 args=parser.parse_args()
@@ -29,10 +35,17 @@ for row in reader:
 	rooms[unicode(row[1])]=unicode(row[0])
 
 start_time = args.starttime
-end_time = args.endtime
+if not args.endtime:
+	start = datetime.datetime.strptime( start_time, "%Y-%m-%dT%H:%M:%S" )
+	end_time = (start + datetime.timedelta(hours=1)).isoformat()
+else:
+	end_time = args.endtime
 
 user = args.user
 password = getpass.getpass("Password:")
+
+print "Searching for a room from " + start_time + " to " + end_time + ":"
+print "{0:10s} {1:64s} {2:64s}".format("Status", "Room", "Email")
 
 xml_template = open("getavailibility_template.xml", "r").read()
 xml = Template(xml_template)
@@ -45,7 +58,7 @@ for room in rooms:
 
 	tree = ET.fromstring(response)
 
-	status="Free"
+	status = "Free"
 	# arrgh, namespaces!!
 	elems=tree.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}BusyType")
 	for elem in elems:
