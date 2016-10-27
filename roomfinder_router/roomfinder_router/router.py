78a9701162c355e104035697d7694a71c10cba5a
Guismo1 <guismo1@free.fr>
2016-10-27 12:06:50 +0200

    add directory feature through mq

--- roomfinder_router/roomfinder_router/router.py
+++ roomfinder_router/roomfinder_router/router.py
@@ -19,7 +19,40 @@ def on_request(ch, method, props, body):
         txt=page.text
         sys.stderr.write("txt: {}\n".format(txt))    
     elif cmd == "dir":
-        pass
+        sys.stderr.write("Request directory entry in %s\n" % dir_server)  
+        print "dir_server: "+dir_server
+        print "photo_server: "+photo_server
+
+        u = dir_server + cco
+        try:
+            page = requests.get(u)
+        except requests.exceptions.ConnectionError:
+            return "Connection error to directory server"
+        try: 
+            from BeautifulSoup import BeautifulSoup
+        except ImportError:
+            from bs4 import BeautifulSoup
+        html = page.text
+        parsed_html = BeautifulSoup(html)
+        name=parsed_html.body.find('span', attrs={'class':'name'})
+        sys.stderr.write("name: "+str(name)+"\n")
+        if not hasattr(name, 'text'):
+            return "CCO id not found !"
+        else:
+            title=parsed_html.body.find('span', attrs={'class':'title'})
+            sys.stderr.write("title: "+str(title)+"\n")
+            manager=parsed_html.body.find('a', attrs={'class':'hover-link'})
+            sys.stderr.write("manager: "+str(manager)+"\n")
+            
+            u = photo_server + cco + ".jpg"
+            txt=""
+            with open('/app/output.jpg', 'wb') as handle:
+                response = requests.get(u, stream=True)
+                if response.ok:
+                    for block in response.iter_content(1024):
+                        handle.write(block)    
+                txt=name.text+";"+title.text+";"+manager.text+";"+"/app/output.jpg"+";"+"<a href=\"http://wwwin-tools.cisco.com/dir/details/"+cco+"\">directory link</a>"
+        sys.stderr.write("txt: {}\n".format(txt))    
     elif cmd == "sr":
         pass
     ch.basic_publish(exchange='',
@@ -36,6 +69,12 @@ if __name__ == '__main__':
     parser.add_argument("-r","--rabbitmq", help="IP or hostname for rabbitmq server, e.g. 'rabbit.domain.com'.")
     parser.add_argument("-p","--port", help="tcp port for rabitmq server, e.g. '2765'.")
     parser.add_argument("-b","--book", help="URL for roomfinder book server, e.g. 'http://book.domain.com:1234'.")
+    parser.add_argument(
+        "-d", "--dir", help="Address of directory server", required=False
+    )
+    parser.add_argument(
+        "-i", "--photo", help="Address of photo directory server", required=False
+    )
 #    parser.add_argument("-p","--password", help="password for exchange server.")
     args = parser.parse_args()
 
@@ -59,6 +98,22 @@ if __name__ == '__main__':
         if (book_server == None):
             get_book_server = raw_input("What is the book server URL? ")
             book_server = get_book_server
+
+    dir_server = args.dir
+    # print "Arg Dir: " + str(dir_server)
+    if (dir_server == None):
+        dir_server = os.getenv("roomfinder_dir_server")
+        # print "Env Dir: " + str(dir_server)
+    # print "Dir Server: " + dir_server
+    sys.stderr.write("Directory Server: " + str(dir_server) + "\n")
+
+    photo_server = args.photo
+    # print "Arg Photo: " + str(photo_server)
+    if (photo_server == None):
+        photo_server = os.getenv("roomfinder_photo_server")
+        # print "Env Photo: " + str(photo_server)
+    # print "Photo Server: " + photo_server
+    sys.stderr.write("Directory Photo Server: " + str(photo_server) + "\n")
 
     sys.stderr.write("Connecting to "+rabbitmq+" on port "+rabbitmq_port+"\n")
     connection = pika.BlockingConnection(pika.ConnectionParameters(
