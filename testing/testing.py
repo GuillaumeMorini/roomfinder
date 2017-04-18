959ce7fc370ead531bb58b6ac03a059d913f9584
Guismo1 <guismo1@free.fr>
2017-04-18 22:24:45 +0200

    first commit

---
+++ testing/testing.py
@@ -0,0 +1,41 @@
+import unittest
+import sys
+import os
+sys.path.append('roomfinder_web/roomfinder_web')
+import web_server
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        sys.stderr.write('Setup testing.\n')
+        web_server.data_server = os.getenv("roomfinder_data_server")
+        web_server.book_url = os.getenv("roomfinder_book_server")
+        web_server.app.config['TESTING'] = True
+        self.app = web_server.app.test_client()
+ 
+    def test_correct_http_response(self):
+        sys.stderr.write('Test HTTP GET / == 200.\n')
+        resp = self.app.get('/')
+        self.assertEquals(resp.status_code, 200)
+    def test_about_correct_http_response(self):
+        sys.stderr.write('Test HTTP GET /about == 200.\n')
+        resp = self.app.get('/about')
+        self.assertEquals(resp.status_code, 200)
+    def test_form_correct_http_response(self):
+        sys.stderr.write('Test HTTP GET /form == 200.\n')
+        resp = self.app.get('/form')
+        self.assertEquals(resp.status_code, 200)
+
+    # def test_correct_content(self):
+    #     resp = self.app.get('/hello/world')
+    #     self.assertEquals(resp.data, '"Hello World!"\n')
+
+    # def test_universe_correct_content(self):
+    #     resp = self.app.get('/hello/universe')
+    #     self.assertEquals(resp.data, '"Hello Universe!"\n')
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
