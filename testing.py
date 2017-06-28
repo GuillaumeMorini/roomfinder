bbd3338b51f064eb25aa8057bc12f4587194f121
Guismo1 <guismo1@free.fr>
2017-06-28 20:29:56 +0200

    modify testing for roomfinder_spark

--- testing.py
+++ testing.py
@@ -8,23 +8,23 @@ class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
         sys.stderr.write('Setup testing.\n')
-        web_server.data_server = os.getenv("roomfinder_data_server")
-        web_server.book_url = os.getenv("roomfinder_book_server")
-        web_server.app.config['TESTING'] = True
-        self.app = web_server.app.test_client()
+        #web_server.data_server = os.getenv("roomfinder_data_server")
+        #web_server.book_url = os.getenv("roomfinder_book_server")
+        spark_bot.app.config['TESTING'] = True
+        self.app = spark_bot.app.test_client()
  
     def test_correct_http_response(self):
-        sys.stderr.write('Test HTTP GET / == 200.\n')
-        resp = self.app.get('/')
+        sys.stderr.write('Test HTTP GET /demoroom/members == 200.\n')
+        resp = self.app.get('/demoroom/members')
         self.assertEquals(resp.status_code, 200)
-    def test_about_correct_http_response(self):
-        sys.stderr.write('Test HTTP GET /about == 200.\n')
-        resp = self.app.get('/about')
-        self.assertEquals(resp.status_code, 200)
-    def test_form_correct_http_response(self):
-        sys.stderr.write('Test HTTP GET /form == 200.\n')
-        resp = self.app.get('/form')
-        self.assertEquals(resp.status_code, 200)
+    #def test_about_correct_http_response(self):
+    #    sys.stderr.write('Test HTTP GET /about == 200.\n')
+    #    resp = self.app.get('/about')
+    #    self.assertEquals(resp.status_code, 200)
+    #def test_form_correct_http_response(self):
+    #    sys.stderr.write('Test HTTP GET /form == 200.\n')
+    #    resp = self.app.get('/form')
+    #    self.assertEquals(resp.status_code, 200)
 
     # def test_correct_content(self):
     #     resp = self.app.get('/hello/world')
